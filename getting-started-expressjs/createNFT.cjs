813d54623708b50f9b9f91a0c5237d5014330754
Loris Leiva <loris.leiva@gmail.com>
2022-06-24 14:09:16 +0100

    chore: update readme and bundlr address

--- getting-started-expressjs/createNFT.cjs
+++ getting-started-expressjs/createNFT.cjs
@@ -1,28 +1,31 @@
-const { Metaplex, keypairIdentity, mockStorage } = require("@metaplex-foundation/js");
+const {
+  Metaplex,
+  keypairIdentity,
+  bundlrStorage,
+} = require("@metaplex-foundation/js");
 const { Connection, clusterApiUrl, Keypair } = require("@solana/web3.js");
-const fs = require('fs');
+const fs = require("fs");
 
-const pathToMyKeypair = process.env.HOME + '/.config/solana/id.json';
+const pathToMyKeypair = process.env.HOME + "/.config/solana/id.json";
 const keypairFile = fs.readFileSync(pathToMyKeypair);
 const secretKey = Buffer.from(JSON.parse(keypairFile.toString()));
 const myKeyPair = Keypair.fromSecretKey(secretKey);
 
-const connection = new Connection(clusterApiUrl('devnet'));
+const connection = new Connection(clusterApiUrl("devnet"));
 const metaplex = Metaplex.make(connection)
   .use(keypairIdentity(myKeyPair))
-  .use(mockStorage());
+  .use(bundlrStorage({ address: "https://devnet.bundlr.network" }));
 
-let uploadNFT = async () => {
+const uploadNFT = async () => {
+  const { uri } = await metaplex.nfts().uploadMetadata({
+    name: "First NFT",
+  });
 
-    const { uri } = await metaplex.nfts().uploadMetadata({
-        name: 'First NFT',
-    });
+  const { nft } = await metaplex.nfts().create({
+    uri: uri,
+  });
 
-    const {nft} = await metaplex.nfts().create({
-        uri:uri
-    });
-
-    console.log(nft.mint.toBase58());
-}
+  console.log(nft.mint.toBase58());
+};
 
 uploadNFT();
