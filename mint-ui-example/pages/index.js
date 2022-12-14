17c1071b69b930fa6184115cfef1c8fe726720ac
Mark Sackerberg <mark.sackerberg@metaplex.com>
2022-12-14 22:59:22 +0100

    lint

--- mint-ui-example/pages/index.js
+++ mint-ui-example/pages/index.js
@@ -1,48 +1,50 @@
-import styles from '../styles/Home.module.css'
-import { useMemo, useState, useEffect } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import styles from "../styles/Home.module.css";
+import { useMemo, useState, useEffect } from "react";
 import {
-    GlowWalletAdapter,
-    PhantomWalletAdapter,
-    SlopeWalletAdapter,
-    SolflareWalletAdapter,
-    TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
-    WalletModalProvider,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-import { MetaplexProvider } from './MetaplexProvider';
-import { MintNFTs } from './MintNFTs';
-import '@solana/wallet-adapter-react-ui/styles.css';
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { MetaplexProvider } from "./MetaplexProvider";
+import { MintNFTs } from "./MintNFTs";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function Home() {
-
   const [network, setNetwork] = useState(WalletAdapterNetwork.Devnet);
 
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   const wallets = useMemo(
-      () => [
-          new PhantomWalletAdapter(),
-          new GlowWalletAdapter(),
-          new SlopeWalletAdapter(),
-          new SolflareWalletAdapter({ network }),
-          new TorusWalletAdapter(),
-      ],
-      [network]
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
   );
 
   const handleChange = (event) => {
-    switch(event.target.value){
+    switch (event.target.value) {
       case "devnet":
         setNetwork(WalletAdapterNetwork.Devnet);
         break;
       case "mainnet":
         setNetwork(WalletAdapterNetwork.Mainnet);
-      break;
+        break;
       case "testnet":
         setNetwork(WalletAdapterNetwork.Testnet);
         break;
@@ -52,24 +54,20 @@ export default function Home() {
     }
   };
 
-
   return (
     <div>
       <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-        <MetaplexProvider>
-          <div className={styles.App}>
-            <WalletMultiButton />
-            <MintNFTs onClusterChange={handleChange} />
-          </div>
-        </MetaplexProvider>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  </div>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <MetaplexProvider>
+              <div className={styles.App}>
+                <WalletMultiButton />
+                <MintNFTs onClusterChange={handleChange} />
+              </div>
+            </MetaplexProvider>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
   );
-
-
 }
-
