5109200a85ffbd96f63284edf1d233e6e5b7574b
Mark Sackerberg <mark.sackerberg@metaplex.com>
2023-01-04 22:40:10 +0100

    fix hydration error

--- mint-ui-example/pages/index.js
+++ mint-ui-example/pages/index.js
@@ -20,6 +20,7 @@ import { clusterApiUrl } from "@solana/web3.js";
 import { MetaplexProvider } from "./MetaplexProvider";
 import { MintNFTs } from "./MintNFTs";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import dynamic from 'next/dynamic'
 
 export default function Home() {
   const [network, setNetwork] = useState(WalletAdapterNetwork.Devnet);
@@ -54,6 +55,11 @@ export default function Home() {
     }
   };
 
+  const ButtonWrapper = dynamic(() =>
+  import('@solana/wallet-adapter-react-ui').then((mod) => mod.WalletMultiButton)
+)
+
+
   return (
     <div>
       <ConnectionProvider endpoint={endpoint}>
@@ -61,7 +67,7 @@ export default function Home() {
           <WalletModalProvider>
             <MetaplexProvider>
               <div className={styles.App}>
-                <WalletMultiButton />
+                <ButtonWrapper />
                 <MintNFTs onClusterChange={handleChange} />
               </div>
             </MetaplexProvider>
